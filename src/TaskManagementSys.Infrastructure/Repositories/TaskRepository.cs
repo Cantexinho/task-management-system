d6d05d7559404e902bb76c0acb157c84b02d9586
Cantexinho <pk.developing@gmail.com>
2025-05-05 18:31:35 +0300

    Added TaskRepository

---
+++ src/TaskManagementSys.Infrastructure/Repositories/TaskRepository.cs
@@ -0,0 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManagementSys.Core.Entities;
+using TaskManagementSys.Infrastructure.Data;
+
+namespace TaskManagementSys.Infrastructure.Repositories
+{
+    public class TaskRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TaskRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<TaskItem>> GetAllTasksAsync()
+        {
+            return await _context.Tasks.ToListAsync();
+        }
+
+        public async Task<TaskItem?> GetTaskByIdAsync(int id)
+        {
+            return await _context.Tasks.FindAsync(id);
+        }
+
+        public async Task<TaskItem> CreateTaskAsync(TaskItem task)
+        {
+            task.CreatedAt = DateTime.Now;
+            _context.Tasks.Add(task);
+            await _context.SaveChangesAsync();
+            return task;
+        }
+
+        public async Task<TaskItem> UpdateTaskAsync(TaskItem task)
+        {
+            _context.Entry(task).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return task;
+        }
+
+        public async Task<bool> DeleteTaskAsync(int id)
+        {
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+                return false;
+
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
