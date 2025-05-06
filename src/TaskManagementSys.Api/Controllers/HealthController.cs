57555739d59b447d98c218ad2acfc25c17e87ad7
Cantexinho <pk.developing@gmail.com>
2025-05-06 17:49:53 +0300

    Updated HealthController to be able to return database schema for dev

--- src/TaskManagementSys.Api/Controllers/HealthController.cs
+++ src/TaskManagementSys.Api/Controllers/HealthController.cs
@@ -4,7 +4,6 @@ using TaskManagementSys.Core.Entities;
 using TaskManagementSys.Infrastructure.Data;
 using TaskManagementSys.Api.Dtos;
 
-
 namespace TaskManagementSys.Api.Controllers
 {
     [ApiController]
@@ -62,6 +61,20 @@ namespace TaskManagementSys.Api.Controllers
                 long? fileSize = fileExists ? new FileInfo(dataSource!).Length : null;
                 DateTime? lastModified = fileExists ? new FileInfo(dataSource!).LastWriteTime : null;
 
+                // Get database schema information
+                var schemaInfo = new Dictionary<string, List<TableColumnInfo>>();
+                
+                if (canConnect)
+                {
+                    var tables = GetAllTables();
+                    
+                    foreach (var table in tables)
+                    {
+                        var columns = GetTableColumns(table);
+                        schemaInfo[table] = columns;
+                    }
+                }
+
                 return Ok(new HealthCheckResponse
                 {
                     Status = canConnect ? "Connected" : "Disconnected",
@@ -72,6 +85,7 @@ namespace TaskManagementSys.Api.Controllers
                     FileSize = fileSize,
                     LastModified = lastModified,
                     TaskCount = taskCount,
+                    DatabaseSchema = schemaInfo
                 });
             }
             catch (Exception ex)
@@ -79,6 +93,87 @@ namespace TaskManagementSys.Api.Controllers
                 _logger.LogError(ex, "Health check failed");
                 return StatusCode(500, new { Error = ex.Message });
             }
+        }
+
+        [HttpGet("schema")]
+        public IActionResult GetDatabaseSchema()
+        {
+            try
+            {
+                var schemaInfo = new Dictionary<string, List<TableColumnInfo>>();
+                var tables = GetAllTables();
+                
+                foreach (var table in tables)
+                {
+                    var columns = GetTableColumns(table);
+                    schemaInfo[table] = columns;
+                }
+
+                return Ok(schemaInfo);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to retrieve database schema");
+                return StatusCode(500, new { Error = ex.Message });
+            }
+        }
+
+        private List<string> GetAllTables()
+        {
+            // SQLite specific query to get all tables
+            var tables = new List<string>();
+            
+            using (var command = _context.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = "SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%' AND name NOT LIKE 'ef_%';";
+                
+                _context.Database.OpenConnection();
+                
+                using (var result = command.ExecuteReader())
+                {
+                    while (result.Read())
+                    {
+                        tables.Add(result.GetString(0));
+                    }
+                }
+            }
+            
+            return tables;
+        }
+
+        private List<TableColumnInfo> GetTableColumns(string tableName)
+        {
+            var columns = new List<TableColumnInfo>();
+            
+            using (var command = _context.Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = $"PRAGMA table_info('{tableName}');";
+                
+                if (_context.Database.GetDbConnection().State != System.Data.ConnectionState.Open)
+                {
+                    _context.Database.OpenConnection();
+                }
+                
+                using (var result = command.ExecuteReader())
+                {
+                    while (result.Read())
+                    {
+                        var columnInfo = new TableColumnInfo
+                        {
+                            ColumnId = result.GetInt32(0),
+                            Name = result.GetString(1),
+                            Type = result.GetString(2),
+                            NotNull = result.GetBoolean(3),
+                            DefaultValue = result.IsDBNull(4) ? null : result.GetString(4),
+                            PrimaryKey = result.GetInt32(5) > 0
+                        };
+                        
+                        columns.Add(columnInfo);
+                    }
+                }
+            }
+            
+            return columns;
         }
     }
 }
