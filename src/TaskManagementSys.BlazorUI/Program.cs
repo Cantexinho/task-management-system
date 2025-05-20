e987080800d0bddbbc0e78feca30972866e4c85d
Cantexinho <pk.developing@gmail.com>
2025-05-20 18:17:54 +0300

    Added auth services to blazor program

--- src/TaskManagementSys.BlazorUI/Program.cs
+++ src/TaskManagementSys.BlazorUI/Program.cs
@@ -1,9 +1,23 @@
 using TaskManagementSys.BlazorUI.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using TaskManagementSys.BlazorUI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuthenticationCore();
+builder.Services.AddScoped<ProtectedSessionStorage>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<HttpClient>(sp => new HttpClient 
+{ 
+    BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"] ?? "https://localhost:7077") 
+});
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddCascadingAuthenticationState();
 
 var app = builder.Build();
 
@@ -15,7 +29,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
